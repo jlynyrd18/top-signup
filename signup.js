fc3bfe5eab2ea84240b6d4fd402f163023882bca
Jordan Lenard <leonard_2010@live.com>
2023-06-14 22:02:15 -0500

    notes on what i need to validate

--- signup.js
+++ signup.js
@@ -3,3 +3,7 @@ document.getElementById("create").addEventListener("click", createAccount);
 function createAccount() {
     
 }
+//Need to validate if a box is empty, if is then pop up that all boxes are required
+//Validate email is actually an email
+//validate password and confirm password are the same
+//could put line with "Fields marked with an asterisk are required"/ if do asterisk need p tags for right below that would pop up and say required
