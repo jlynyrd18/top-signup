2f35d01d26751cee48f54d82ad478aba2b6ccd8b
Jordan Lenard <leonard_2010@live.com>
2023-06-26 21:58:54 -0500

    added if statments for checking email and password

--- signup.js
+++ signup.js
@@ -5,6 +5,10 @@
 submitButton.addEventListener("click", submit);
 
  function submit() {
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+    const confirm = document.getElementById("cpassword");
+
     for (let x = 0; x < needForm.length; x++) {
         var value = needForm[x].value;
         if (value == "") {
@@ -12,10 +16,22 @@ submitButton.addEventListener("click", submit);
             for (let i = 0; i < paragraphs.length; i++) {
                 paragraphs[i].style.visibility = "visible";
             }
+        } if (!emailIsValid(emailInput.value)) {
+            alert("Email is not valid");
+            break;
+        } if (passwordInput.value !== confirm.value) {
+            alert("Passwords do not match. Please try again");
+            break;
+        } if (x === needForm.length - 1){
+            alert("Account Created");
+            
         }
     }
  }
-
+function emailIsValid(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
 //Need to validate if a box is empty, if is then pop up that all boxes are required
 //Validate email is actually an email
 //validate password and confirm password are the same
