235d8b6389e7ec11e78912ca6ec88d8bf24cb81d
Jordan Lenard <leonard_2010@live.com>
2023-06-26 21:30:43 -0500

    created function and change for if input is empty

--- signup.js
+++ signup.js
@@ -1,8 +1,21 @@
-document.getElementById("create").addEventListener("click", createAccount);
+ const needForm = document.querySelectorAll("input[required]");
+ const submitButton = document.getElementById("create"); 
+ const paragraphs = document.getElementsByTagName("p");
 
-function createAccount() {
-    
-}
+submitButton.addEventListener("click", submit);
+
+ function submit() {
+    for (let x = 0; x < needForm.length; x++) {
+        var value = needForm[x].value;
+        if (value == "") {
+            needForm[x].style.borderColor = "red";
+            for (let i = 0; i < paragraphs.length; i++) {
+                paragraphs[i].style.visibility = "visible";
+            }
+        }
+    }
+ }
+
 //Need to validate if a box is empty, if is then pop up that all boxes are required
 //Validate email is actually an email
 //validate password and confirm password are the same
